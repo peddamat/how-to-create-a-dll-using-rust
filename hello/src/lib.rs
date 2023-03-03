4a2bb8026cb5169c74dfbf046a0dbb7e74e3d6d5
Sam Peddamatham <peddamat@gmail.com>
2023-03-03 14:54:26 -0800

    Added DllMain skeleton.

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -1,3 +1,22 @@
+use windows::{ Win32::Foundation::*, Win32::System::SystemServices::*, };
+
+#[no_mangle]
+#[allow(non_snake_case, unused_variables)]
+extern "system" fn DllMain(
+    dll_module: HINSTANCE,
+    call_reason: u32,
+    _: *mut ())
+    -> bool
+{
+    match call_reason {
+        DLL_PROCESS_ATTACH => (),
+        DLL_PROCESS_DETACH => (),
+        _ => ()
+    }
+
+    true
+}
+
 #[no_mangle]
 pub extern fn add(left: usize, right: usize) -> usize {
     left + right
