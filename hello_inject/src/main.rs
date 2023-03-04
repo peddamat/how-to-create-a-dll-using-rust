ab38cf779f8a8fc81c0cc5f4ffc23c2f3f9c6e82
Sam Peddamatham <peddamat@gmail.com>
2023-03-04 00:05:48 -0800

    Added dll-syringe example code.

--- hello_inject/src/main.rs
+++ hello_inject/src/main.rs
@@ -1,3 +1,24 @@
+use dll_syringe::{Syringe, process::OwnedProcess};
+use std::{thread, time};
+
 fn main() {
-    println!("Hello, world!");
+    // find target process by name
+    let target_process =
+	    OwnedProcess::find_first_by_name("create_window.exe").unwrap();
+
+    // create a new syringe for the target process
+    let syringe = Syringe::for_process(target_process);
+
+    // inject the payload into the target process
+    let injected_payload = syringe.inject("target\\debug\\hello.dll").unwrap();
+    println!("DLL injected successfully!");
+
+    // do something else
+    let ten_millis = time::Duration::from_secs(30);
+
+    println!("Sleeping for 30 secs...");
+    thread::sleep(ten_millis);
+
+    // eject the payload from the target (optional)
+    syringe.eject(injected_payload).unwrap();
 }
